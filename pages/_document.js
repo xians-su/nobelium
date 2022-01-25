7356f29a392d7b18f466cd24145182a76b158f36
Xians <github@xians.su>
2022-01-26 03:04:10 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -88,7 +88,7 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" sizes="192x192" href="/apple-touch-icon.png"></link>
           <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="/feed"></link>
-          {BLOG.appearance === 'auto'
+          {/* {BLOG.appearance === 'auto'
             ? (
             <>
             <meta name="theme-color" content={BLOG.lightBackground} media="(prefers-color-scheme: light)"/>
@@ -98,7 +98,7 @@ class MyDocument extends Document {
             : (
             <meta name="theme-color" content={BLOG.appearance === 'dark' ? BLOG.darkBackground : BLOG.lightBackground} />
               )
-          }
+          } */}
         </Head>
         <body className="bg-day dark:bg-night">
           <Main />
