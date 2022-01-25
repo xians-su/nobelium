f4986b67624e4989fb4ab446f7a67e1eef9fbc2f
Xians <github@xians.su>
2022-01-26 03:29:49 +0800

    Update TagItem.js

--- components/TagItem.js
+++ components/TagItem.js
@@ -1,13 +1,13 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const TagItem = ({ tag }) => (
   <Link href={`/tag/${encodeURIComponent(tag)}`}>
     <a>
-      <p className="mr-1 rounded-full px-2 py-1 border leading-none text-sm dark:border-gray-600">
+      <p className="mr-2 rounded-full px-2 py-1 border leading-none text-sm hover:bg-gray-100 dark:border-gray-600 dark:hover:bg-gray-700 transition-colors duration-200">
         {tag}
       </p>
     </a>
   </Link>
-)
+);
 
-export default TagItem
+export default TagItem;
