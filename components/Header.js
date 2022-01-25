7f934b6ac5cc223d2b7d68c66889864dc18892c1
Xians <github@xians.su>
2022-01-25 18:36:55 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -8,9 +8,8 @@ const NavBar = () => {
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: 'https://xians.su', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.PLAYGROUND, to: '/playground', show: PLAYGROUND.showPlayground },
-    { id: 3, name: locale.NAV.RSS, to: '/feed', show: true },
-    { id: 4, name: locale.NAV.SEARCH, to: '/search', show: true }
+    { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
+    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
     <div className="flex-shrink-0">
