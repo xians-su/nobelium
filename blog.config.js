1041b8216426b9526ee8d45625b612efba9d4acd
Xians <github@xians.su>
2022-01-25 18:36:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,7 +14,6 @@ const BLOG = {
   postsPerPage: 7,
   sortByDate: false,
   showAbout: true,
-  showPlayground: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app/**%E6%85%A2%E4%B8%89%E6%8B%8D%E5%98%AE%E5%8F%A8**%20by%20Xians.png?theme=light&md=1&fontSize=150px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg', // The link to generate OG image, don't end with a slash https://og-image-craigary.vercel.app
