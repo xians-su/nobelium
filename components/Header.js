87c8b6d045ca7e8def808585d7064b515397fdd3
Xians <github@xians.su>
2022-01-25 18:24:20 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -7,10 +7,9 @@ const NavBar = () => {
   const locale = useLocale()
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.PLAYGROUND, to: '/playground', show: true },
-    { id: 2, name: locale.NAV.ABOUT, to: 'https://xians.su', show: BLOG.showAbout },
-    { id: 3, name: locale.NAV.RSS, to: '/feed', show: true },
-    { id: 4, name: locale.NAV.SEARCH, to: '/search', show: true }
+    { id: 1, name: locale.NAV.ABOUT, to: 'https://xians.su', show: BLOG.showAbout },
+    { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
+    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
     <div className="flex-shrink-0">
