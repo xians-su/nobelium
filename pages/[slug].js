27122557b755e01cb062eee49e7614636adea424
Xians <github@xians.su>
2022-01-26 03:07:32 +0800

    Update [slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,10 +1,18 @@
-import Layout from '@/layouts/layout'
-import { getAllPosts, getPostBlocks } from '@/lib/notion'
-import BLOG from '@/blog.config'
-import { createHash } from 'crypto'
+import Layout from "@/layouts/layout";
+import { getAllPosts, getPostBlocks } from "@/lib/notion";
+import BLOG from "@/blog.config";
+import { createHash } from "crypto";
+import Container from "@/components/Container";
+import Error from "@/components/Error";
 
 const BlogPost = ({ post, blockMap, emailHash }) => {
-  if (!post) return null
+  if (!post) {
+    return (
+      <Container>
+        <Error errCode={400} title={"頁面不存在."} />
+      </Container>
+    );
+  }
   return (
     <Layout
       blockMap={blockMap}
@@ -12,31 +20,31 @@ const BlogPost = ({ post, blockMap, emailHash }) => {
       emailHash={emailHash}
       fullWidth={post.fullWidth}
     />
-  )
-}
+  );
+};
 
-export async function getStaticPaths () {
-  const posts = await getAllPosts({ includePages: true })
+export async function getStaticPaths() {
+  const posts = await getAllPosts({ includePages: true });
   return {
-    paths: posts.map(row => `${BLOG.path}/${row.slug}`),
-    fallback: true
-  }
+    paths: posts.map((row) => `${BLOG.path}/${row.slug}`),
+    fallback: true,
+  };
 }
 
-export async function getStaticProps ({ params: { slug } }) {
-  const posts = await getAllPosts({ includePages: true })
-  const post = posts.find(t => t.slug === slug)
-  const blockMap = await getPostBlocks(post.id)
-  const emailHash = createHash('md5')
+export async function getStaticProps({ params: { slug } }) {
+  const posts = await getAllPosts({ includePages: true });
+  const post = posts.find((t) => t.slug === slug) ?? null;
+  const blockMap = post ? await getPostBlocks(post.id) : null;
+  const emailHash = createHash("md5")
     .update(BLOG.email)
-    .digest('hex')
+    .digest("hex")
     .trim()
-    .toLowerCase()
+    .toLowerCase();
 
   return {
     props: { post, blockMap, emailHash },
-    revalidate: 1
-  }
+    revalidate: 1,
+  };
 }
 
-export default BlogPost
+export default BlogPost;
