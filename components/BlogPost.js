4c95c7f19a5324c616a2d2f5270780a2561b78bc
Xians <github@xians.su>
2022-01-26 03:18:48 +0800

    Update BlogPost.js

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,18 +1,24 @@
-import Link from 'next/link'
-import BLOG from '@/blog.config'
-import formatDate from '@/lib/formatDate'
+import Link from "next/link";
+import BLOG from "@/blog.config";
+import formatDate from "@/lib/formatDate";
 
 const BlogPost = ({ post }) => {
   return (
     <Link href={`${BLOG.path}/${post.slug}`}>
       <a>
-        <article key={post.id} className="mb-6 md:mb-8">
+        <article
+          key={post.id}
+          className="mb-6 lg:p-4 md:p-4 sm:p-0 rounded-lg lg:hover:bg-gray-200 lg:hover:bg-opacity-40 lg:dark:hover:bg-gray-800 lg:dark:hover:bg-opacity-50 md:hover:bg-gray-200 md:hover:bg-opacity-40 md:dark:hover:bg-gray-800 md:dark:hover:bg-opacity-50 transition-colors duration-200"
+        >
           <header className="flex flex-col justify-between md:flex-row md:items-baseline">
             <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
               {post.title}
             </h2>
             <time className="flex-shrink-0 text-gray-600 dark:text-gray-400">
-              {formatDate(post?.date?.start_date || post.createdTime, BLOG.lang)}
+              {formatDate(
+                post?.date?.start_date || post.createdTime,
+                BLOG.lang
+              )}
             </time>
           </header>
           <main>
@@ -23,7 +29,7 @@ const BlogPost = ({ post }) => {
         </article>
       </a>
     </Link>
-  )
-}
+  );
+};
 
-export default BlogPost
+export default BlogPost;
