11a4fd35fdc56cddc7b08150ce490d813a1f324a
Xians <github@xians.su>
2022-01-26 01:35:02 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -68,7 +68,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
         <div className="flex items-center">
           <Link href="/">
           <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-//            <a aria-label={BLOG.title}>
+            <a aria-label={BLOG.title}>
 //              <div className="h-6">
 //                <svg
 //                  width="24"
