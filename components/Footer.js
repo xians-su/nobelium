ca2909a6e2c1f9d93f539cfdcdfbf0ecb35e0a8e
Xians <github@xians.su>
2022-01-26 12:39:56 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,29 +1,26 @@
-import classNames from 'classnames';
-import BLOG from '~/blog.config';
-import Vercel from '~/components/Vercel';
-
-type Props = { fullWidth?: boolean };
-
-export const Footer: React.VFC<Props> = ({ fullWidth }) => {
-  const d = new Date();
-  const y = d.getFullYear();
-  const from = +BLOG.since;
+import BLOG from '@/blog.config'
+import Vercel from '@/components/Vercel'
+const Footer = ({ fullWidth }) => {
+  const d = new Date()
+  const y = d.getFullYear()
+  const from = +BLOG.since
   return (
     <div
-      className={classNames('mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all', {
-        'px-4 md:px-24': fullWidth,
-        'max-w-2xl px-4': !fullWidth,
-      })}
+      className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
+        !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
+      }`}
     >
       <hr className="border-gray-200 dark:border-gray-600" />
       <div className="my-4 text-sm leading-6">
-        <div className="flex flex-wrap justify-between align-baseline">
+        <div className="flex align-baseline justify-between flex-wrap">
           <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`} - Build with Next.js & Notion
+            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
           </p>
           <Vercel />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
+
+export default Footer
