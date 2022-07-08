fd331474c7e4f29505a4f0e87c2e30a08ce3f36c
Xians <github@xians.su>
2022-07-08 21:24:01 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -28,7 +28,7 @@ const BLOG = {
     provider: 'ackee', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
       tracker: 'https://ackee.xians.su/tracker.js', // e.g 'https://ackee.craigary.net/tracker.js'
-      dataAckeeServer: 'https://ackee.xians.su/', // e.g https://ackee.craigary.net , don't end with a slash
+      dataAckeeServer: 'https://ackee.xians.su', // e.g https://ackee.craigary.net , don't end with a slash
       domainId: 'a531dcad-6197-4326-9abe-3f2d2d35b463' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
