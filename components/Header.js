90d7eeb3412eee1d5b1f7b66200b8a1025166be1
Xians <github@xians.su>
2022-09-28 16:35:10 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -154,8 +154,8 @@ const Header = ({ navBarTitle, fullWidth }) => {
               <div className='h-6'>
                 <Image
                   src='/top.png'
-                  width={26}
-                  height={26}
+                  width={18.34}
+                  height={18.34}
                   alt='xians'
                 />
               </div>
