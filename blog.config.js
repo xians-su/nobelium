fee4fe62ab92f943cb847f10e03b015130054d7d
Xians <github@xians.su>
2024-01-01 23:43:33 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -50,7 +50,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: '355ed70b-9b33-4f8d-8c7b-db0460517abb', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
