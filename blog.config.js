8411e465196231c38179a63466285ef8f6ae8d80
Xians <github@xians.su>
2024-01-03 23:21:12 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://raw.githubusercontent.com/xians-su/nobelium-notion-blog/main/public/webcover.png',
+  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://xians.su',
   seo: {
     keywords: ['xians', 'Xians Su', 'slowmur', '慢叨', '興趣使然的分心學家'],
