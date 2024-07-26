4cbd4246eeeb00ccc5ac397f27cb7ae7925414f8
Xians <github@xians.su>
2024-07-26 10:17:35 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -167,7 +167,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
             </p>
           ) : (
             <p className="ml-4 font-medium text-day dark:text-night header-name">
-              {BLOG.title},{"💬"}
+              {BLOG.title},{" "}
               <span className="font-normal">{BLOG.description}</span>
             </p>
           )}
